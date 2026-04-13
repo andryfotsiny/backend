e457d2327a7d6961d6d32f9371679af618350f52
Andry <andry.michel1410@gmail.com>
2026-04-13 10:50:04 +0200

    update token

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,8 +9,8 @@ class Settings(BaseSettings):
 
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 43200
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 365
 
     DB_TYPE: str = "postgresql+asyncpg"
     DB_USER: str = "dyleth"
